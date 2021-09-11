c8d8cbf33ddb30e81dc74a447bcba7f0dcbc1e46
Vlad Gheorghiu <vsoftco@gmail.com>
2021-09-11 17:57:59 -0400

    /usr/bin/env

--- scripts/algo2_stats.sh
+++ scripts/algo2_stats.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # $1 - Number of qubits
 # $2 - T-depth target
--- scripts/avg_std.sh
+++ scripts/avg_std.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # $@ - List of result files produced by algo2
 
